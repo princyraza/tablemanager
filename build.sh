422de531adbc493f4b16b1ca044d00248bba4da8
princy <princy.raza@gmail.com>
2019-06-05 08:52:27 +0200

    Upgraded to PyQt5

--- build.sh
+++ build.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 
-pyuic4 -o tableManagerUi.py tableManagerUi.ui
-pyuic4 -o tableManagerUiClone.py tableManagerUiClone.ui
-pyuic4 -o tableManagerUiInsert.py tableManagerUiInsert.ui
-pyuic4 -o tableManagerUiRename.py tableManagerUirename.ui
-pyrcc4 -o resources_rc.py resources.qrc
+pyuic5 -o tableManagerUi.py tableManagerUi.ui
+pyuic5 -o tableManagerUiClone.py tableManagerUiClone.ui
+pyuic5 -o tableManagerUiInsert.py tableManagerUiInsert.ui
+pyuic5 -o tableManagerUiRename.py tableManagerUirename.ui
+pyrcc5 -o resources_rc.py resources.qrc
--- tableManagerUiRename.py
+++ tableManagerUiRename.py
@@ -2,66 +2,52 @@
 
 # Form implementation generated from reading ui file 'tableManagerUirename.ui'
 #
-# Created: Wed Jul 23 07:13:42 2014
-#      by: PyQt4 UI code generator 4.10.4
+# Created by: PyQt5 UI code generator 5.10.1
 #
 # WARNING! All changes made in this file will be lost!
 
-from PyQt4 import QtCore, QtGui
-
-try:
-    _fromUtf8 = QtCore.QString.fromUtf8
-except AttributeError:
-    def _fromUtf8(s):
-        return s
-
-try:
-    _encoding = QtGui.QApplication.UnicodeUTF8
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig, _encoding)
-except AttributeError:
-    def _translate(context, text, disambig):
-        return QtGui.QApplication.translate(context, text, disambig)
+from PyQt5 import QtCore, QtGui, QtWidgets
 
 class Ui_Rename(object):
     def setupUi(self, Rename):
-        Rename.setObjectName(_fromUtf8("Rename"))
+        Rename.setObjectName("Rename")
         Rename.resize(397, 126)
-        self.gridlayout = QtGui.QGridLayout(Rename)
-        self.gridlayout.setObjectName(_fromUtf8("gridlayout"))
-        self.vboxlayout = QtGui.QVBoxLayout()
-        self.vboxlayout.setObjectName(_fromUtf8("vboxlayout"))
-        self.label = QtGui.QLabel(Rename)
-        sizePolicy = QtGui.QSizePolicy(QtGui.QSizePolicy.Preferred, QtGui.QSizePolicy.Fixed)
+        self.gridlayout = QtWidgets.QGridLayout(Rename)
+        self.gridlayout.setObjectName("gridlayout")
+        self.vboxlayout = QtWidgets.QVBoxLayout()
+        self.vboxlayout.setObjectName("vboxlayout")
+        self.label = QtWidgets.QLabel(Rename)
+        sizePolicy = QtWidgets.QSizePolicy(QtWidgets.QSizePolicy.Preferred, QtWidgets.QSizePolicy.Fixed)
         sizePolicy.setHorizontalStretch(0)
         sizePolicy.setVerticalStretch(0)
         sizePolicy.setHeightForWidth(self.label.sizePolicy().hasHeightForWidth())
         self.label.setSizePolicy(sizePolicy)
-        self.label.setObjectName(_fromUtf8("label"))
+        self.label.setObjectName("label")
         self.vboxlayout.addWidget(self.label)
-        self.lineEdit = QtGui.QLineEdit(Rename)
+        self.lineEdit = QtWidgets.QLineEdit(Rename)
         self.lineEdit.setMouseTracking(False)
-        self.lineEdit.setInputMask(_fromUtf8(""))
+        self.lineEdit.setInputMask("")
         self.lineEdit.setMaxLength(10)
         self.lineEdit.setFrame(True)
-        self.lineEdit.setObjectName(_fromUtf8("lineEdit"))
+        self.lineEdit.setObjectName("lineEdit")
         self.vboxlayout.addWidget(self.lineEdit)
         self.gridlayout.addLayout(self.vboxlayout, 0, 0, 1, 1)
-        self.buttonBox = QtGui.QDialogButtonBox(Rename)
+        self.buttonBox = QtWidgets.QDialogButtonBox(Rename)
         self.buttonBox.setOrientation(QtCore.Qt.Horizontal)
-        self.buttonBox.setStandardButtons(QtGui.QDialogButtonBox.Cancel|QtGui.QDialogButtonBox.NoButton|QtGui.QDialogButtonBox.Ok)
+        self.buttonBox.setStandardButtons(QtWidgets.QDialogButtonBox.Cancel|QtWidgets.QDialogButtonBox.NoButton|QtWidgets.QDialogButtonBox.Ok)
         self.buttonBox.setCenterButtons(True)
-        self.buttonBox.setObjectName(_fromUtf8("buttonBox"))
+        self.buttonBox.setObjectName("buttonBox")
         self.gridlayout.addWidget(self.buttonBox, 2, 0, 1, 1)
-        spacerItem = QtGui.QSpacerItem(20, 40, QtGui.QSizePolicy.Minimum, QtGui.QSizePolicy.Expanding)
+        spacerItem = QtWidgets.QSpacerItem(20, 40, QtWidgets.QSizePolicy.Minimum, QtWidgets.QSizePolicy.Expanding)
         self.gridlayout.addItem(spacerItem, 1, 0, 1, 1)
 
         self.retranslateUi(Rename)
-        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL(_fromUtf8("accepted()")), Rename.accept)
-        QtCore.QObject.connect(self.buttonBox, QtCore.SIGNAL(_fromUtf8("rejected()")), Rename.reject)
+        self.buttonBox.accepted.connect(Rename.accept)
+        self.buttonBox.rejected.connect(Rename.reject)
         QtCore.QMetaObject.connectSlotsByName(Rename)
 
     def retranslateUi(self, Rename):
-        Rename.setWindowTitle(_translate("Rename", "Rename field", None))
-        self.label.setText(_translate("Rename", "Enter new field name:", None))
+        _translate = QtCore.QCoreApplication.translate
+        Rename.setWindowTitle(_translate("Rename", "Rename field"))
+        self.label.setText(_translate("Rename", "Enter new field name:"))
 
